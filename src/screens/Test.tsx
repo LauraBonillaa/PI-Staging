db1680f98b5ee89a343efc8c6aec7947a1920e7e
LauraBonillaa <laurasofiabonilla@gmail.com>
2024-10-16 15:20:57 -0500

    feat

---
+++ src/screens/Test.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+export default function Test() {
+    return (
+        <>
+            <h1>Test</h1>
+            <h1>Conoce tu nivel de estrés</h1>
+            <p>Tómate unos minutos para descubrir cómo el estrés está afectando tu bienestar. Completa este sencillo test y recibe sugerencias personalizadas para mejorar tu bienestar.</p>
+            <input type="text" name="nombre" placeholder="Ingresa tu correo"/>
+            
+            <Link to="/form">
+                <button>Continuar</button>
+            </Link>
+        </>
+    )
+}
